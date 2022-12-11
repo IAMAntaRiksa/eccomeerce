e69aa6e7b32057c844a8ae16452f5de0003410f7
IAMAntaRiksa <imamakbarmegaantariksa@gmail.com>
2022-12-11 11:24:15 +0700

    update

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,7 +2,9 @@ import { defineStore } from 'pinia'
 import Api from '../api/api'
 
 export const useCartStore = defineStore('cart', {
-    state() => ({
+    state: () => ({
+        state: {
 
+        }
     })
 })
